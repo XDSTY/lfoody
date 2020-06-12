0bf23d8f041a9f09e65c302e8758597fd637915e
zhangfuhua <zhangfuhua@zuche.com>
2020-06-12 17:50:47 +0800

    init

---
+++ src/pages/cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    cart
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+      return {
+        value : ''
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
